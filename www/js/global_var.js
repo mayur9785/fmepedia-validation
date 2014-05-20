810cca93602ad66840c9fd44d1d6fb20d864aed6
amundhak <amundhak@gmail.com>
2014-05-20 09:24:17 -0700

    Upgraded to new Javascript API plus Clean Up

--- www/js/global_var.js
+++ www/js/global_var.js
@@ -1,11 +1,1 @@
-// Including this document in the index.html file:
-// <script type="text/javascript" src="../global_var.js"></script>
-
-// Example for using the vars in HTML
-// document.write('<a href="mailto:cad_waterqa@' + fmeserverurl +'" target="_blank"> cad_waterqa@' + fmeserverurl +'</a>');
-
-// Example for using in JavaScript
-// pUrlBase = "http://" + fmeserverurl + "/fmejobsubmitter/Demos/D014-ResetOracleTable.fmw"
-
-var fmeserverurl = "demo.fmeserver.com";
-var token = "b75f8edfb40f95a8e41e11b293da7813e7fb9b06"
+var fmeserverurl = "fmepedia2014-safe-software.fmecloud.com";
